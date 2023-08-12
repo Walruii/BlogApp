c8ed53b676a053abd0c01d7f73c2ad57f6cb868e
walruii <walruiix@gmail.com>
2023-08-12 13:03:44 +0530

    ../

---
+++ models/post.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  }
+});
+
+
+mongoose.models = {};
+const Post = mongoose.model("Post", postSchema);
+export default Post
