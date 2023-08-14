a54ca77d1ad8b068706ec51f8d671b67db53ba32
walruii <walruiix@gmail.com>
2023-08-14 11:16:30 +0530

    further refactor

--- app/posts.tsx
+++ app/posts.tsx
@@ -6,16 +6,26 @@ export interface Post {
   content: string;
 }
 
-const getPosts = async () => {
-  const response = await fetch('http://localhost:3000/api/posts', {
-    cache: 'no-store',
-    headers: {
-      'Content-Type': 'application/json',
+export const getPosts = async () => {
+  try {
+    const response = await fetch('http://localhost:3000/api/posts', {
+      cache: 'no-store',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch posts');
     }
-  });
-  const data = await response.json();
-  return data.posts;
-}
+
+    const data = await response.json();
+    return data.posts;
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+    return [];
+  }
+};
 
 export default async function Posts() {
   const posts = await getPosts();
@@ -23,12 +33,15 @@ export default async function Posts() {
     <div className="post-content">
       {posts.map(({ _id, title, content }: Post) => {
         return (
-          <div>
+          <div key={_id}>
             <h1 className="post-title">{title}</h1>
             {content.length < 100 ? (
               <p className="post-content">{content}</p>
             ) : (
-              <p className="post-content">{content.slice(0, 100)}<Link href={`/posts/${_id}`}> ... Read More</Link></p>
+              <>
+                <p className="post-content">{content.slice(0, 100)}</p>
+                <Link href={`/posts/${_id}`}> ... Read More</Link>
+              </>
             )}
           </div>
         )
